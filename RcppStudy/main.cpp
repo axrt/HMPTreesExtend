aa1915fa501ed4048cf13df10c2805aa9c991a0a
alext <alexander.tuzhikov@gmail.com>
2014-09-29 19:05:55 -0400

    Initial Commit

---
+++ RcppStudy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RcppStudy
+//
+//  Created by Alexander Tuzhikov on 9/29/14.
+//  Copyright (c) 2014 Alexander Tuzhikov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
